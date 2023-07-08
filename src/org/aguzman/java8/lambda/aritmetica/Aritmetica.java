b5b95b08539a4eb886ff111da5ebabda7b06ddb1
EduardoSTL <eduardojosemelgar2004@gmail.com>
2023-07-08 15:20:49 -0600

    Initial commit

---
+++ src/org/aguzman/java8/lambda/aritmetica/Aritmetica.java
@@ -0,0 +1,7 @@
+package org.aguzman.java8.lambda.aritmetica;
+
+@FunctionalInterface
+//@FunctionalInterface documentamos la clase interface
+public interface Aritmetica {
+    double operacion(double a, double b);
+}
